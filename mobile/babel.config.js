de19cbe5008e873736cff49cd3420a2f8cfbf203
hassanmubiru <hassanmubiru7@gmail.com>
2025-11-21 06:08:32 +0300

    Changes to be committed: 	new file:   mobile/App.tsx.template 	new file:   mobile/android/app/build.gradle 	new file:   mobile/android/app/debug.keystore 	new file:   mobile/android/app/proguard-rules.pro 	new file:   mobile/android/app/src/main/AndroidManifest.xml 	new file:   mobile/android/app/src/main/java/com/triviatemp/MainActivity.kt 	new file:   mobile/android/app/src/main/java/com/triviatemp/MainApplication.kt 	new file:   mobile/android/app/src/main/res/drawable/rn_edit_text_material.xml 	new file:   mobile/android/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   mobile/android/app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   mobile/android/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   mobile/android/app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   mobile/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   mobile/android/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   mobile/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   mobile/android/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   mobile/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   mobile/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   mobile/android/app/src/main/res/values/strings.xml 	new file:   mobile/android/app/src/main/res/values/styles.xml 	new file:   mobile/android/build.gradle 	new file:   mobile/android/gradle.properties 	new file:   mobile/android/gradle/wrapper/gradle-wrapper.jar 	new file:   mobile/android/gradle/wrapper/gradle-wrapper.properties 	new file:   mobile/android/gradlew 	new file:   mobile/android/gradlew.bat 	new file:   mobile/android/settings.gradle 	modified:   mobile/babel.config.js 	modified:   mobile/index.js 	new file:   mobile/ios/.xcode.env 	new file:   mobile/ios/Podfile 	new file:   mobile/ios/TriviaTemp.xcodeproj/project.pbxproj 	new file:   mobile/ios/TriviaTemp.xcodeproj/xcshareddata/xcschemes/TriviaTemp.xcscheme 	new file:   mobile/ios/TriviaTemp/AppDelegate.swift 	new file:   mobile/ios/TriviaTemp/Images.xcassets/AppIcon.appiconset/Contents.json 	new file:   mobile/ios/TriviaTemp/Images.xcassets/Contents.json 	new file:   mobile/ios/TriviaTemp/Info.plist 	new file:   mobile/ios/TriviaTemp/LaunchScreen.storyboard 	new file:   mobile/ios/TriviaTemp/PrivacyInfo.xcprivacy 	modified:   mobile/metro.config.js 	modified:   mobile/package.json 	modified:   mobile/tsconfig.json

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@': './src',
-          '@components': './src/components',
-          '@services': './src/services',
-          '@screens': './src/screens',
-          '@constants': './src/constants',
-          '@utils': './src/utils',
-          '@types': './src/types',
-        },
-      },
-    ],
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
-
