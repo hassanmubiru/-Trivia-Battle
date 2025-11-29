30b5c8041aa72f4fd82a11ade1c35f151dfbb29f
hassanmubiru <hassanmubiru7@gmail.com>
2025-11-29 08:35:44 +0300

    Changes to be committed: 	modified:   mobile/trivia-battle-expo/package-lock.json 	modified:   mobile/trivia-battle-expo/package.json 	modified:   mobile/trivia-battle-expo/react-native.d.ts 	modified:   mobile/trivia-battle-expo/tsconfig.json

--- mobile/trivia-battle-expo/react-native.d.ts
+++ mobile/trivia-battle-expo/react-native.d.ts
@@ -1,13 +1,33 @@
 declare module 'react-native' {
   import * as React from 'react';
   
+  export type ViewStyle = any;
+  export type TextStyle = any;
+  export type StyleProp<T> = T | any[];
+  
   export interface ViewProps extends React.PropsWithChildren {
-    style?: any;
+    style?: StyleProp<ViewStyle>;
     [key: string]: any;
   }
   
   export interface TextProps extends React.PropsWithChildren {
-    style?: any;
+    style?: StyleProp<TextStyle>;
+    [key: string]: any;
+  }
+  
+  export interface TextInputProps extends React.PropsWithChildren {
+    placeholder?: string;
+    placeholderTextColor?: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    secureTextEntry?: boolean;
+    keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad';
+    editable?: boolean;
+    multiline?: boolean;
+    numberOfLines?: number;
+    onFocus?: () => void;
+    onBlur?: () => void;
+    style?: StyleProp<TextStyle>;
     [key: string]: any;
   }
   
@@ -19,7 +39,10 @@ declare module 'react-native' {
   
   export const View: React.FC<ViewProps>;
   export const Text: React.FC<TextProps>;
+  export const TextInput: React.FC<TextInputProps>;
   export const ActivityIndicator: React.FC<ActivityIndicatorProps>;
   export const TouchableOpacity: React.FC<any>;
-  export const StyleSheet: any;
+  export const StyleSheet: {
+    create: (styles: Record<string, any>) => Record<string, any>;
+  };
 }
